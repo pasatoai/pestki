dade85b4775ededc07736dddf1aa9fffe8c591da
mainshum <aleksander@aims.ai>
2022-09-11 20:19:33 +0200

    Edit mode started

--- pages/gabinet/[city].tsx
+++ pages/gabinet/[city].tsx
@@ -2,27 +2,61 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
+import { isErrored } from "stream";
 import * as L from "../../components/Layout/layout";
 import styles from "./gabinetpage.module.css";
 
 type Decoration = "underline" | "none" | "italic";
 
-type Block = {};
+type Block = { text: string; decoration: Decoration };
 
 // each section is two subscections
 
 type Section = {
   key: number;
-  leftS?: SubSection;
-  rightS?: SubSection;
+  leftS: SubSection;
+  rightS: SubSection;
 };
 
 type SubSection = {
-  name: string;
-  blocks: Block[];
+  name?: string;
+  blocks?: Block[];
 };
 
-function Block() {}
+function EditBlock({ isEdit, text, decoration }: { isEdit: boolean } & Block) {
+  return isEdit ? <input defaultValue={text} /> : <span>{text}</span>;
+}
+
+function Block({ text, decoration, isEdit }: Block & { isEdit: boolean }) {
+  return isEdit ? <input defaultValue={text} type="text" /> : null;
+}
+
+function SubSection({ name, blocks }: SubSection) {
+  const [isEdit, setIsEdit] = useState(false);
+
+  return (
+    <div className={styles.subSection}>
+      <input className={styles.subName} defaultValue={name} type="text" />
+      <EditBlock
+        text={name || "Nowa sekcja"}
+        isEdit={isEdit}
+        decoration={"none"}
+      />
+      <p className={styles.padder} />
+      <section className={styles.subBlocks}>
+        {blocks?.map(({ text, decoration }) => (
+          <EditBlock
+            key={text}
+            text={text}
+            isEdit={isEdit}
+            decoration={decoration}
+          />
+        ))}
+        {isEdit && <button>Dodaj blok</button>}
+      </section>
+    </div>
+  );
+}
 
 function GabinetPage(x: any) {
   const { query } = useRouter();
@@ -30,14 +64,24 @@ function GabinetPage(x: any) {
   const [sections, setSections] = useState<Section[]>([
     {
       key: Math.random(),
-      leftS: { name: "Urofizjo", blocks: [] },
+      leftS: {
+        name: "Urofizjo",
+        blocks: [
+          { text: "Olek przeszlo", decoration: "italic" },
+          { text: "Olek to ja", decoration: "italic" },
+          { text: "doesit@gmail.com", decoration: "italic" },
+        ],
+      },
       rightS: { name: "Urofizjo2", blocks: [] },
     },
     {
       key: Math.random(),
       leftS: { name: "Urofizjo", blocks: [] },
+      rightS: {},
     },
   ]);
+
+  const onBlockAdded = (sectionKey: number, isLeftSub: boolean, b: Block) => {};
 
   return (
     <L.Page isViolet={true}>
@@ -58,8 +102,8 @@ function GabinetPage(x: any) {
       <L.Content className={styles.sectionsWrapper}>
         {sections.map(({ key: sk, leftS, rightS }) => (
           <div key={sk} className={styles.section}>
-            <div className={styles.subSection}>{leftS?.name}</div>
-            <div className={styles.subSection}>{rightS?.name}</div>
+            <SubSection name={leftS?.name} blocks={leftS?.blocks} />
+            <SubSection name={rightS?.name} blocks={rightS?.blocks} />
           </div>
         ))}
       </L.Content>
